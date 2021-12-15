3c2078d74bb924a4dc2671e28580c63e8f36fb6f
gitcnk <58092026+gitcnk@users.noreply.github.com>
2021-12-14 20:24:49 -0500

    Add files via upload

---
+++ Stat152/Final.R
@@ -0,0 +1,6 @@
+file_address1 <-'https://raw.githubusercontent.com/gitcnk/Data/master/Stat152/survey_data.rda'
+file_address2 <-'https://raw.githubusercontent.com/gitcnk/Data/master/Stat152/FERTRATE.rda'
+
+load(url(file_address1))
+
+load(url(file_address2))
